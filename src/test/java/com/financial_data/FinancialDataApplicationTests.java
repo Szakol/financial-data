732fec368424d9e7e595f6a63f958d0ac008cc03
Kacper Piątek <kapistereo@gmail.com>
2024-10-25 08:03:35 +0200

    Initial commit

---
+++ src/test/java/com/financial_data/FinancialDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financial_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
